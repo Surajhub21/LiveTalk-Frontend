119df243cb21440160f4fe273c6e9dbc89d71777
Suraj Mondal <sm.surojmondal@gmail.com>
2025-10-30 13:55:21 +0530

    Ready Deploy

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Global configuration
 window.ENV = {
-    API_URL: "https://a3982f63469c64b399c528b1a353a4d1-466322157.ap-south-1.elb.amazonaws.com:9090/",
-    WS_URL: "http://ad9d3524fb1d14237a468048c5fbcca4-1542234690.ap-south-1.elb.amazonaws.com:8083/websocket"
+    API_URL: "https://localhost:9090/",
+    WS_URL: "https://localhost:8083/websocket"
 };
 
 
